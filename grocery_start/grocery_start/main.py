a57fb249dbffbf0d4bec53f0f0c3f1b8b8f6ba15
AlanLespron <31513436+AlanLespron@users.noreply.github.com>
2023-08-25 23:21:50 -0600

    Add files via upload

--- grocery_start/grocery_start/main.py
+++ grocery_start/grocery_start/main.py
@@ -1,27 +1,43 @@
-
 import csv
 
 
-# data = [{'name': 'John Doe', 'age': 30}, {'name': 'Jane Doe', 'age': 25}]
 def write_list_of_dicts_to_csv(filename, data):
-    with open(filename, 'w') as f:
-        writer = csv.DictWriter(f, fieldnames=data[0].keys())
-        writer.writeheader()
-        writer.writerows(data)
+  with open(filename, 'w', newline='') as f:
+    writer = csv.DictWriter(f, fieldnames=data[0].keys())
+    writer.writeheader()
+    writer.writerows(data)
 
 
 def read_csv_to_dict(filename):
-    with open(filename, 'r') as f:
-        reader = csv.DictReader(f)
-        return list(reader)
+  with open(filename, 'r') as f:
+    reader = csv.DictReader(f)
+    return list(reader)
 
 
-def main(filename):
-    data = read_csv_to_dict(filename)
-    for row in data:
-        print(row)
+def main(sample_filename, batch_filename, output_filename):
+  # Leer los datos del archivo sample_grocery.csv
+  sample_data = read_csv_to_dict(sample_filename)
+  sample_dict = {row['SKU']: row for row in sample_data}
+
+  # Leer los datos del archivo grocery_batch_1.csv
+  batch_data = read_csv_to_dict(batch_filename)
+
+  # Comparar y actualizar los datos
+  for row in batch_data:
+    sku = row['SKU']
+    cantidad = int(row['Quantity'])
+
+    if sku in sample_dict:
+      sample_dict[sku]['Quantity'] = str(
+        int(sample_dict[sku]['Quantity']) + cantidad)
+    else:
+      sample_dict[sku] = row
+
+  # Guardar los datos actualizados en el archivo grocery_db.csv
+  write_list_of_dicts_to_csv(output_filename, list(sample_dict.values()))
+
+  print("Proceso completado. Datos actualizados guardados en", output_filename)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    main('sample_grocery.csv')
+  main('sample_grocery.csv', 'grocery_batch_1.csv', 'grocery_db.csv')
